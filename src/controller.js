43c9758ec9ffda04e3dc9c1243769dcaea1e3cb5
Iryna <ielkinairinka@gmail.com>
2022-02-13 21:09:34 +0300

    +

--- src/controller.js
+++ src/controller.js
@@ -2,10 +2,11 @@ export default class Controller {
 	constructor(game, view) {
 		this.game = game;
 		this.view = view;
+		this.intervalId = null;
+		this.isPlaying = false;
 
-		setInterval(() => {
-			this.update();
-		}, 1000);
+
+
 
 		document.addEventListener('keydown', this.handleKeyDown.bind(this));
 		this.view.renderStartScreen();
@@ -13,13 +14,55 @@ export default class Controller {
 
 	update() {
 		this.game.movePieceDown();
-		this.view.renderMainScreen(this.game.getState());
+		this.updateView();
+	}
+
+	play() {
+		this.isPlaying = true;
+		this.startTimer();
+		this.updateView();
+
+	}
+
+	pause() {
+		this.isPlaying = false;
+		this.stopTimer();
+		this.updateView();
+	}
+
+	updateView() {
+		if (!this.isPlaying) {
+			this.view.renderPauseScreen();
+		} else {
+			this.view.renderMainScreen(this.game.getState());
+		}
+	}
+
+	startTimer() {
+		if (!this.intervalId) {
+			this.intervalId = setInterval(() => {
+				this.update();
+			}, 1000);
+		}
+	}
+
+	stopTimer() {
+		if (this.intervalId) {
+			clearInterval(this.intervalId);
+			this.intervalId = null;
+		}
 	}
 
 	handleKeyDown(event) {
 		switch (event.keyCode) {
 			case 13: //enter
-				this.view.renderMainScreen(this.game.getState());
+				if (this.isPlaying) {
+					this.pause();
+				} else {
+					this.play();
+				}
+				break;
+			// this.view.renderMainScreen(this.game.getState());
 			case 37: //left
 				this.game.movePieceLeft();
 				this.view.renderMainScreen(game.getState());
